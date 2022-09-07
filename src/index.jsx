f4f91a8011e5cab386ad5f3032f1a246930f22b4
wsz <wsz@arch.home>
2022-09-08 00:52:06 +0200

    /

--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,95 @@
-import React         from 'react';
-import ReactDOM      from 'react-dom/client';
+//# *************************************************************************** #
+//                                                                              #
+//                                                         :::      ::::::::    #
+//    index.jsx                                          :+:      :+:    :+:    #
+//                                                     +:+ +:+         +:+      #
+//    By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+         #
+//                                                 +#+#+#+#+#+   +#+            #
+//    Created: 2022/09/07 22:53:05 by wszurkow          #+#    #+#              #
+//    Updated: 2022/09/07 22:53:24 by wszurkow         ###   ########.fr        #
+//                                                                              #
+//# *************************************************************************** #
+
+// =============================================================================
+// ---------------------------------- IMPORTS ----------------------------------
+// =============================================================================
+
+import React           from 'react';
+import ReactDOM        from 'react-dom/client';
+
 import {Marquee}       from "react-fast-marquee";
 import {useMediaQuery} from 'react-responsive';
 //import {Collapsible}   from 'react-collapsible';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./global.css"
 
-function test (){
-	return (<div>hello morld</div>)
-}
+// =============================================================================
+// ----------------------------------- MEDIA -----------------------------------
+// =============================================================================
+
+//import IMAGE_VAR_NAME from './img/..'
+
+
+
+
+
+// =============================================================================
+// -------------------------- DEFINITION OF VARIABLES --------------------------
+// =============================================================================
+
+const BREAKPOINT = 800; // Value of responsive breakpoint (width) in px
+
+
+
 
 // =============================================================================
 // ---------------------------------- MOBILE -----------------------------------
 // =============================================================================
 
+const Mobile = () => {
+	return (
+		<div className="flex col bgdusk">
+		<div className="flex fillv bggrass">
+			ok
+		</div>
+		<div className="flex fillv bggrass">
+			ok
+		</div>
+		</div>
+	);
+};
 
 // =============================================================================
 // ---------------------------------- DESKTOP ----------------------------------
 // =============================================================================
 
+const Desktop = () => {
+	return (
+		<div className="flex fill bggrass">
+			ok
+		</div>
+	);
+};
 
 // =============================================================================
 // ----------------------------------- MAIN ------------------------------------
 // =============================================================================
 
+function Main() {
+	const isMobile  = useMediaQuery({query: '(max-width:'+ BREAKPOINT     + 'px)'})
+	const isDesktop = useMediaQuery({query: '(min-width:'+ (BREAKPOINT + 1) + 'px)'})
+	console.log('(max-width:'+ (BREAKPOINT+1) + 'px)')
+	return (
+		<div className="flex fill">
+			{ isMobile  && <Mobile/>  }
+			{ isDesktop && <Desktop/> }
+		</div>
+	)
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-	  {test()}
-  </React.StrictMode>
+	<React.StrictMode>
+		<Main/>
+	</React.StrictMode>
 );
